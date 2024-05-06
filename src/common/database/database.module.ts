c7987ea022a9f82bbe66d1a9c42303d1961775dc
hoamx <hoa.mx@kiotviet.com>
2024-05-06 08:36:47 +0700

    feat: setup mongodb database

---
+++ src/common/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class DatabaseModule {}
