2bcff6ceca0da7c0ea7bb4a3d8f447cab1dd3340
hoamx <hoa.mx@kiotviet.com>
2024-05-06 11:12:25 +0700

    feat: add validation pipe

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,10 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Field()
+  @IsStrongPassword()
   password: string;
 }
