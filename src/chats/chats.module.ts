a61c650f335cdcf2578b6b7863606278214b1f60
hoamx <hoa.mx@kiotviet.com>
2024-05-07 14:48:34 +0700

    fix: resolve dependence

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChatsService } from './chats.service';
 import { ChatsResolver } from './chats.resolver';
 import { ChatsRepository } from './chats.repository';
+import { DatabaseModule } from 'src/common/database/database.module';
+import { Chat, ChatsSchema } from './entities/chat.entity';
 
 @Module({
+  imports: [
+    DatabaseModule.forFeature([{ name: Chat.name, schema: ChatsSchema }]),
+  ],
   providers: [ChatsResolver, ChatsService, ChatsRepository],
 })
 export class ChatsModule {}
