2b719fde2f16618ebbb7768f45059b3926fbedc0
hoamx <hoamx.work@gmail.com>
2024-05-07 22:52:52 +0700

    feat: add createMessage resolver

---
+++ src/chats/messages/messages.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { ChatsRepository } from '../chats.repository';
+import { Message } from './entities/message.entity';
+import { Types } from 'mongoose';
+import { CreateMessageInput } from './dto/create-message.input';
+
+@Injectable()
+export class MessagesService {
+  constructor(private readonly chatsRepository: ChatsRepository) {}
+
+  async createMessage({ content, chatId }: CreateMessageInput, userId: string) {
+    const message: Message = {
+      content,
+      userId,
+      createdAt: new Date(),
+      _id: new Types.ObjectId(),
+    };
+    await this.chatsRepository.findOneAndUpdate(
+      {
+        _id: chatId,
+        $or: [
+          {
+            userId,
+          },
+          {
+            userIds: {
+              $in: [userId],
+            },
+          },
+        ],
+      },
+      {
+        $push: {
+          messages: message,
+        },
+      },
+    );
+
+    return message;
+  }
+}
